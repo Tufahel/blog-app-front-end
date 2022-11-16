b85c911761f2ff7cca6c19156a14a02785237c0b
Tufahel <tufahel96@gmail.com>
2022-11-16 13:32:03 +0600

    fix responsiveness

--- src/components/Post/Posts.js
+++ src/components/Post/Posts.js
@@ -26,7 +26,7 @@ const Posts = () => {
       <div className="flex flex-col m-2">
         <h2 className="ml-6 text-3xl font-bold">Latest Post</h2>
         {latestPost?.map((post) => (
-          <div className="flex flex-row ml-6 mt-2 pb-4 space-x-12 border-b-2" key={maxId}>
+          <div className="flex flex-col md:flex-row ml-6 mt-2 pb-4 space-x-12 border-b-2 md:shrink-0" key={maxId}>
             <img className="rounded object-cover h-56" src={post.image} alt="img" />
             <div className="flex flex-col justify-center border-b-3">
               <h4 className="font-medium text-lg text-4xl">
@@ -34,7 +34,7 @@ const Posts = () => {
               </h4>
               <div className="flex">
                 <p className="w-60 truncate">{post.text}</p>
-                <NavLink to="/post"><button className="text-green-500" type="button" onClick={() => setPostId(post.post_id)}>See more</button></NavLink>
+                <NavLink to="/post"><button className="text-green-500" type="button" onClick={() => setPostId(post.post_id)}>more</button></NavLink>
               </div>
               <LikeCommentCount id={post.post_id} />
               <div>
