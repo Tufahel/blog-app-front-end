c9d6c8fdad52f3033faddbcb0f636c3979ff748d
Tufahel <tufahel96@gmail.com>
2022-11-27 22:27:06 +0600

    backend deployed in render, url changed

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'https://limitless-gorge-05434.herokuapp.com';
+const URL = 'https://blog-app-ror.onrender.com';
 const userId = localStorage.getItem('userid');
 
 export const fetchUserData = async () => {
